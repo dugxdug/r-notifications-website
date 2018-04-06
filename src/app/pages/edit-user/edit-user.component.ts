50631449cf8948bef9af6e74b62b9f8a26cda142
Tim McGee <tmcgee@reliaslearning.com>
2018-04-06 11:07:25 -0400

    Updating edit users and adding styling

--- src/app/pages/edit-user/edit-user.component.ts
+++ src/app/pages/edit-user/edit-user.component.ts
@@ -3,6 +3,7 @@ import { MatTableDataSource } from '@angular/material';
 import { UsersService } from '../../services/users/users.service';
 import { DepartmentService } from '../../services/departments/departments.service';
 import { User } from '../../services/users/users.dto';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -21,7 +22,8 @@ export class EditUserPageComponent implements OnInit {
 
     departments: Array<string> = [];
     constructor(private usersService: UsersService,
-    private departmentService: DepartmentService) {
+    private departmentService: DepartmentService,
+    private route: ActivatedRoute) {
         this.user = new User();
     }
 
@@ -32,11 +34,13 @@ export class EditUserPageComponent implements OnInit {
             });
         });
 
-        this.usersService.GetUserByEmail('dcoble@relias.com').subscribe((res) => {
-            this.selectedDepartment = res.departments[0];
-            this.selectedPerference = res.notificationPref;
-            this.selectedName = res.email;
-            this.selectedEmail = res.name;
+        let email;
+        this.route.params.subscribe(params => email = params.email);
+        this.usersService.GetUserByEmail(email).subscribe((res) => {
+            this.selectedDepartment = res[0].departments[0];
+            this.selectedPerference = res[0].notificationPref;
+            this.selectedName = res[0].email;
+            this.selectedEmail = res[0].name;
         });
     }
 
@@ -45,12 +49,15 @@ export class EditUserPageComponent implements OnInit {
     }
 
     updateUser() {
-        console.log(this.selectedName);
         this.user.departments.push(this.selectedDepartment);
         this.user.email = this.selectedName;
         this.user.name = this.selectedEmail;
         this.user.notificationPref = this.selectedPerference;
 
-        this.usersService.updateUser(this.user);
+        this.usersService.updateUser(this.user).subscribe(res => {
+            console.log(res);
+        }, error => {
+            console.log(error);
+        });
     }
 }
--- src/app/pages/notification/notification.component.ts
+++ src/app/pages/notification/notification.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     // tslint:disable-next-line:component-selector
