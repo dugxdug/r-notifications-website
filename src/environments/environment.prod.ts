af574d225233f7cf06c162b04080c637096e9139
Chris Walsh <cwalsh@reliaslearning.com>
2018-04-06 11:57:00 -0400

    adding env settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: true,
   name: 'Production',
-  reliasAlertsApi: 'https://r-notifications-api.azurewebsites.net/api/v1/'
+  reliasAlertsApi: 'https://r-notifications-api.azurewebsites.net/api/v1/',
+  firebaseConfig: {
+    apiKey: "AIzaSyCiPaOM5Nr4B9ssl2I4I0VY1Ql_mAi5eTQ",
+    authDomain: "relias-notifications-hack.firebaseapp.com",
+    databaseURL: "https://relias-notifications-hack.firebaseio.com",
+    projectId: "relias-notifications-hack",
+    storageBucket: "relias-notifications-hack.appspot.com",
+    messagingSenderId: "242528928798"
+  }
 };
